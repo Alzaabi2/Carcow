bb605bf528e0780964e170fb49954437c94777c6
alzaabi2 <alzaabi>
2022-09-26 19:03:28 -0400

    .

--- chrome_extension_code/script.js
+++ chrome_extension_code/script.js
@@ -1,6 +1,14 @@
+const chaiAsPromised = require("chai-as-promised");
+
 async function fetchData() {
-    const urlString = window.location.search;
-    if (console.log(urlParams.has('www.cars.com'))){
+    const urlString = 'www.cars.com';
+    chrome.tabs.query({active: true, currentWindow:
+    true}, function(tabs) {
+    var tab = tabs[0];
+    console.log(tab.url);
+    });
+    
+    if (console.log(urlString)){
         const record = await res.json();
         console.log(record);
         document.getElementById("make").innerHTML=record.data[0].make;
@@ -12,4 +20,6 @@ async function fetchData() {
     else{
 
     }
-}fetchData();
+}
+
+fetchData();
