abded6cc0e008316d2e2711bba6cb6cd3dd40cb9
alzaabi2 <alzaabi>
2022-10-04 18:42:59 -0400

    scrape multiple pages

--- scrapeV1,1.py
+++ scrapeV1,1.py
@@ -10,27 +10,48 @@ from csv import writer
 def Scrape(make, model, year, zipcode):
     make = make.lower()
     model = model.lower()
-    url = 'https://www.cars.com/shopping/results/?stock_type=used&makes%5B%5D=' + make + '&models%5B%5D=' + make + '-' + model +'&list_price_max=&maximum_distance=20&zip=' + zipcode
-    url2 = 'https://www.cars.com/shopping/results/?list_price_max=&makes[]=' + make + '&maximum_distance=20&models[]=' + make + '-' + model +'&page=1&page_size=100&stock_type=used&zip=' + zipcode
-    page = requests.get(url2)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    cars = soup.find_all('div', class_="vehicle-card")
-    # print(page.content)
+    
+    url = 'https://www.cars.com/shopping/results/?list_price_max=&makes[]=' + make + '&maximum_distance=100&models[]=' + make + '-' + model +'&page=1&page_size=100&stock_type=used&zip=' + zipcode
+    # search first 10 pages
     with open('cardata.csv', 'w', encoding='utf8', newline='') as f:
         w = writer(f)
         header = ['Make', 'Model', 'Year', 'Mileage', 'Price']
         w.writerow(header)
-        for c in cars:
-            title = c.find('h2', class_="title").text
-            # print(title)
-            title = title.split(' ', 2)
-            year = title[0]
-            make = title[1]
-            model = title[2]
-            price = c.find('span', class_="primary-price").text
-            mileage = c.find('div', class_="mileage").text
-            row = [make, model, year, mileage, price]
-            w.writerow(row)
+        for n in range(10):
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            cars = soup.find_all('div', class_="vehicle-card")
+            # print(page.content)
+
+        
+            for c in cars:
+                title = c.find('h2', class_="title").text
+                # print(title)
+                title = title.split(' ', 2)
+                year = title[0]
+                make = title[1]
+                model = title[2]
+                price = c.find('span', class_="primary-price").text
+                if not c.find('div', class_="mileage"):
+                    mileage = ' '#assume its brand new??
+                else:
+                    mileage = c.find('div', class_="mileage").text
+                row = [make, model, year, mileage, price]
+                w.writerow(row)
+            url = getNextPage(soup)
+            if url == None:
+                break
+        
+def getNextPage(soup):
+    page = soup.find('div', class_='sds-pagination__controls')
+    next = page.find('button', id="next_paginate")
+    if next == None: 
+        next = page.find('a', id="next_paginate")
+    url = 'http://cars.com' + str(next.get('href'))
+    if url == 'http://cars.comNone':
+        print('no next page')
+        return
+    return url
         
 def ScrapeVin(make,model,year,zipcode):
     make = make.lower()
@@ -81,7 +102,5 @@ def scrapeTrimPrice(make, model, year, trim):
     url = 'https://www.cars.com/research/audi-a3-2018/specs/'
     
 
-ScrapeVin('Audi', 'A7', '2014', '22043')
-
-
+Scrape('Toyota', 'Camry', '2014', '22043')
 
