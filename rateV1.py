450155d788c1d3143bd0ea8a81b46d3fdcb485fc
alzaabi2 <alzaabi>
2022-10-17 23:10:18 -0400

    concurrent dollarvalue()

--- rateV1.py
+++ rateV1.py
@@ -8,20 +8,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
+import concurrent.futures
 
 def main():
 
-    demoDict = [{'Make': 'BMW', 'Model': '228i xDrive', 'Year': '2016', 'Mileage': '65,956', 'Price': '$24,950', 'VIN': 'WBA1G9C51GV599609'},
-                {'Make': 'BMW', 'Model': '228 Gran Coupe i xDrive', 'Year': '2021', 'Mileage': '24,681', 'Price': '$36,450', 'VIN': 'WBA73AK03M7H21242'},
-                {'Make': 'BMW', 'Model': '228 i', 'Year': '2014', 'Mileage': '56,547', 'Price': '$22,590', 'VIN': 'WBA1F5C50EV255231'}]
+    # demoDict = [{'Make': 'BMW', 'Model': '228i xDrive', 'Year': '2016', 'Mileage': '65,956', 'Price': '$24,950', 'VIN': 'WBA1G9C51GV599609'},
+    #             {'Make': 'BMW', 'Model': '228 Gran Coupe i xDrive', 'Year': '2021', 'Mileage': '24,681', 'Price': '$36,450', 'VIN': 'WBA73AK03M7H21242'},
+    #             {'Make': 'BMW', 'Model': '228 i', 'Year': '2014', 'Mileage': '56,547', 'Price': '$22,590', 'VIN': 'WBA1F5C50EV255231'}]
 
-    print(demoDict)
+    # print(demoDict)
     # dollarValue('Audi', 'A7', '2016', 130065, 22701) # Need to know which car from chrome extension
     # dollarValueVin('WAUP2AF20KN116129')
-    rate(demoDict)
+    
+    #runs in 5 seconds per car:
+    print("threading")
+    list = createList()
+    with concurrent.futures.ThreadPoolExecutor() as executer:
+        executer.map(dollarValueVin2, list)  
+ 
     # listOfCars = createList()
     # print(listOfCars)
+    # dollarValueVin('ZHWUC1ZD4ELA02158')
 
 carlist = []
 
@@ -58,7 +65,14 @@ def rate(list):
         price = price.replace(',', '')
         suggested = suggested.replace('$', '')
         price = price.replace('$', '')
-        ratio = int(suggested)/int(price) #metric for rating deal
+        price = price.replace(' ', '')
+        print('no', i, 'p: ',price)
+        
+        try:
+            ratio = int(suggested)/int(price) #metric for rating deal
+        except: 
+            ratio == 'No Ratio'
+            
         row = (vin,ratio)
         deals.append(row)
         print("Ratio: ", ratio)
@@ -144,9 +158,6 @@ def dollarValueVin(vin):
 
     # Click "lookup"
     lookUp = browser.find_element(By.ID, 'instantMarketValueFromVIN_0').click()
-    
-    print("ran")
-
 
     # Ensure that the suggested price is visible
     while(True):
@@ -166,6 +177,52 @@ def dollarValueVin(vin):
                     (end-start) * 10**3, "ms")
             return price
     
+
+### Function that looks up the suggested retail value for
+# the specific vehicle using the VIN passed in ###
+# version 2 takes in a dictionary of car data that holds the VIN instead of a VIN string
+def dollarValueVin2(c):
+    start = time.time()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    
+    browser = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
+    # browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    
+    browser.get('https://www.cargurus.com/Cars/car-valuation')
+
+
+    # Click "Look up by VIN"
+    vinButton = browser.find_element(By.ID, 'searchByVinToggle').click()
+
+    # input vin form
+    vinInput = browser.find_element(By.ID, 'carIdentifier')
+    
+    # input the vin 
+    vinInput.send_keys(c['VIN'])
+
+    # Click "lookup"
+    lookUp = browser.find_element(By.ID, 'instantMarketValueFromVIN_0').click()
+
+    # Ensure that the suggested price is visible
+    while(True):
+        idFound = False
+
+        # To Ensure that the ID 'instantMarketValuePrice' is visible
+        try:
+            price = browser.find_element(By.ID, 'instantMarketValuePrice').text
+            idFound = True
+        except:
+            print("Error finding ID instantMarketValuePrice")
+
+        if (idFound and (price != "$ Calculating..." and price != "$—")):
+            print("Suggested Price from CarGurus:", price)
+            end = time.time()
+            print("The time of execution of above program (VIN) is :",
+                    (end-start) * 10**3, "ms")
+            return price
+
+
 
 if __name__ == '__main__':
     main()
